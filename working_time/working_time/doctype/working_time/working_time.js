8d292e0dc5ea975adf73a5f0980018beb7dac0c1
scdanieli <23150094+scdanieli@users.noreply.github.com>
2023-01-21 04:47:04 +0100

    feat: update Working Time

--- working_time/working_time/doctype/working_time/working_time.js
+++ working_time/working_time/doctype/working_time/working_time.js
@@ -1,8 +1,13 @@
 // Copyright (c) 2023, ALYF GmbH and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Working Time', {
-	// refresh: function(frm) {
+frappe.ui.form.on("Working Time", {});
 
-	// }
+frappe.ui.form.on("Working Time Log", {
+    time_logs_add: function (frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        row.from_time = frappe.datetime.now_time(false);
+        row.to_time = ""; // Otherwise Frappe may overwrite empty values with the current time on save.
+        frm.refresh_field("time_logs");
+    },
 });
--- working_time/working_time/doctype/working_time/working_time.py
+++ working_time/working_time/doctype/working_time/working_time.py
@@ -1,8 +1,45 @@
 # Copyright (c) 2023, ALYF GmbH and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
+from frappe.utils import time_diff_in_seconds
+
 
 class WorkingTime(Document):
-	pass
+    def before_validate(self):
+        self.remove_seconds()
+        self.set_to_times()
+        self.set_durations()
+
+    def validate(self):
+        for log in self.time_logs:
+            if log.duration and log.duration < 0:
+                frappe.throw(_("Time logs must be continuous"))
+
+    def remove_seconds(self):
+        for log in self.time_logs:
+            if log.from_time:
+                log.from_time = f"{log.from_time[:-2]}00"
+
+            if log.to_time:
+                log.to_time = f"{log.to_time[:-2]}00"
+
+    def set_to_times(self):
+        for i in range(0, len(self.time_logs) - 1):
+            self.time_logs[i].to_time = self.time_logs[i + 1].from_time
+
+    def set_durations(self):
+        self.break_time = 0
+        self.working_time = 0
+
+        for log in self.time_logs:
+            if log.from_time and log.to_time:
+                log.duration = time_diff_in_seconds(log.to_time, log.from_time)
+
+            if log.duration:
+                if log.is_break:
+                    self.break_time += log.duration
+                else:
+                    self.working_time += log.duration
