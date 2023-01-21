d5449fa700cd3b9d0bf4849dc3df3049c0f48e14
scdanieli <23150094+scdanieli@users.noreply.github.com>
2023-01-21 06:27:24 +0100

    feat: add Activity Type

--- working_time/install.py
+++ working_time/install.py
@@ -1,12 +1,14 @@
 # Copyright (c) 2023, ALYF GmbH and contributors
 # For license information, please see license.txt
 
+import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 
 def after_install():
     customize_project()
     customize_timesheet()
+    insert_docs()
 
 
 def customize_project():
@@ -63,3 +65,23 @@ def customize_timesheet():
     }
 
     create_custom_fields(custom_fields)
+
+
+def insert_docs():
+    docs = [
+        {
+            "doctype": "Activity Type",
+            "activity_type": "Default",
+        }
+    ]
+
+    for doc in docs:
+        filters = doc.copy()
+
+        # Clean up filters. They need to be a plain dict without nested dicts or lists.
+        for key, value in doc.items():
+            if isinstance(value, (list, dict)):
+                del filters[key]
+
+        if not frappe.db.exists(filters):
+            frappe.get_doc(doc).insert(ignore_if_duplicate=True)
