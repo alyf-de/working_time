ab1a83e4b8299ba42f0a2637304dac281d6d5c37
HenningWendtland <156231187+HenningWendtland@users.noreply.github.com>
2024-05-13 18:46:20 +0200

    refactor: get_filter_values.py

--- working_time/working_time/report/expected_and_actual_working_time/get_filter_values.py
+++ working_time/working_time/report/expected_and_actual_working_time/get_filter_values.py
@@ -1,18 +1,16 @@
 import frappe
 
+
 @frappe.whitelist()
 def get_employee_working_hours(user): # use weekly hours field in employee to calculate daily working time
-    employee = frappe.get_value("Employee", {"user_id": user}, "name")
-    if employee:
-        working_hours_per_week = frappe.get_value("Employee", employee, "working_hours_per_week")
-        if working_hours_per_week:
-            working_hours_per_day = working_hours_per_week / 5
-            return working_hours_per_day
-    return None
+	employee = frappe.get_value("Employee", {"user_id": user}, "name")
+	if not employee:
+		return None
+	working_hours_per_week = frappe.get_value("Employee", employee, "working_hours_per_week")
+	if working_hours_per_week:
+		return working_hours_per_week / 5
+
 
 @frappe.whitelist()
 def get_employee_name(user):
-    employee = frappe.get_value("Employee", {"user_id": user}, "name")
-    if employee:
-        return employee
-    return None
+	return frappe.get_value("Employee", {"user_id": user}, "name")
