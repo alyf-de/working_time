06a7ac2aa6b42351dc2a489e2daf4c56aa99cdad
Henning Wendtland <156231187+HenningWendtland@users.noreply.github.com>
2024-05-17 09:57:52 +0200

    Update working_time/working_time/report/expected_and_actual_working_time/get_filter_values.py
    Co-authored-by: Raffael Meyer <14891507+barredterra@users.noreply.github.com>

--- working_time/working_time/report/expected_and_actual_working_time/get_filter_values.py
+++ working_time/working_time/report/expected_and_actual_working_time/get_filter_values.py
@@ -2,8 +2,12 @@ import frappe
 
 
 @frappe.whitelist()
-def get_employee_working_hours(user): # use weekly hours field in employee to calculate daily working time
-	employee = frappe.get_value("Employee", {"user_id": user}, "name")
+def get_employee_working_hours(employee: str):
+	"""Return the average daily working time calculated from the employee's weekly hours."""
+	if not isinstance(employee, str):
+		raise ValueError("Employee should be a string")
+
+	frappe.has_permission("Employee", throw=True)
 	if not employee:
 		return None
 	working_hours_per_week = frappe.get_value("Employee", employee, "working_hours_per_week")
