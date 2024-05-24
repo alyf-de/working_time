5fd2f4f08db3654599b3f69f0acdbde969e19094
HenningWendtland <156231187+HenningWendtland@users.noreply.github.com>
2024-05-24 16:52:03 +0200

    refactor: move working hour calculation to "on change" section

--- working_time/working_time/report/expected_and_actual_working_time/expected_and_actual_working_time.js
+++ working_time/working_time/report/expected_and_actual_working_time/expected_and_actual_working_time.js
@@ -10,13 +10,27 @@ frappe.query_reports["Expected and Actual Working Time"] = {
 			fieldtype: "Link",
 			options: "Employee",
 			reqd: 1,
+			on_change: function(report){
+				frappe.call({
+					method: "working_time.working_time.report.expected_and_actual_working_time.get_filter_values.get_employee_working_hours",
+					args: {
+						"employee": report.get_filter_value("employee")
+					},
+					callback: (r) => {
+						if (r.message) {
+							var filter = report.get_filter("daily_working_hours");
+							filter.set_value(r.message);
+						}
+					}
+				});
+			}
 		},
 		{
 			fieldname: "from_date",
 			label: __("From Date"),
 			fieldtype: "Date",
 			reqd: 1,
-			default: frappe.datetime.add_days(frappe.datetime.nowdate(), -7),
+			default: frappe.datetime.add_days(frappe.datetime.nowdate(), -30),
 		},
 		{
 			fieldname: "to_date",
@@ -34,19 +48,6 @@ frappe.query_reports["Expected and Actual Working Time"] = {
 	],
 	// fetch filter values for employee and daily_working_hours using db call and python script
 	onload: function(report) {
-		frappe.call({
-			method: "working_time.working_time.report.expected_and_actual_working_time.get_filter_values.get_employee_working_hours",
-			args: {
-				"employee": report.get_filter_value("employee")
-			},
-			callback: (r) => {
-				if (r.message) {
-					var filter = report.get_filter("daily_working_hours");
-					filter.set_value(r.message);
-				}
-			}
-		});
-
 		frappe.call({
 			method: "working_time.working_time.report.expected_and_actual_working_time.get_filter_values.get_employee_name",
 			callback: function(r) {
