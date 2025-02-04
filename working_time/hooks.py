6b939aca4954fef8cd1d49f5d76e37683a5502b4
barredterra <14891507+barredterra@users.noreply.github.com>
2025-02-04 01:22:25 +0100

    feat: send reminders to submit working time

--- working_time/hooks.py
+++ working_time/hooks.py
@@ -106,13 +106,14 @@ after_install = "working_time.install.after_install"
 # Scheduled Tasks
 # ---------------
 
-# scheduler_events = {
+scheduler_events = {
 #	"all": [
 #		"working_time.tasks.all"
 #	],
-#	"daily": [
-#		"working_time.tasks.daily"
-#	],
+	"daily": [
+		"working_time.reminders.send_stale_reminders",
+		"working_time.reminders.send_month_end_reminders",
+	],
 #	"hourly": [
 #		"working_time.tasks.hourly"
 #	],
@@ -122,7 +123,7 @@ after_install = "working_time.install.after_install"
 #	"monthly": [
 #		"working_time.tasks.monthly"
 #	]
-# }
+}
 
 # Testing
 # -------
