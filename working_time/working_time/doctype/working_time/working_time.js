9c8d8a1afd06788e1275dfead30103852c6521c3
barredterra <14891507+barredterra@users.noreply.github.com>
2025-06-09 20:04:42 +0200

    fix: avoid overriding existing key (if it was entered first)

--- working_time/working_time/doctype/working_time/working_time.js
+++ working_time/working_time/doctype/working_time/working_time.js
@@ -90,7 +90,10 @@ frappe.ui.form.on("Working Time Log", {
 		frappe.db
 			.get_value("Project", child.project, ["project_type", "default_key"])
 			.then(({ message }) => {
-				frappe.model.set_value(cdt, cdn, "key", message.default_key);
+				if (!child.key && message.default_key) {
+					frappe.model.set_value(cdt, cdn, "key", message.default_key);
+				}
+
 				if (message && message.project_type == "Internal") {
 					frappe.model.set_value(cdt, cdn, "billable", "0%");
 				} else {
